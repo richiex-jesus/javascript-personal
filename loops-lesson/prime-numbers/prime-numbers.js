5b5d8d748df7ec047551a943f7a5efc742b4b03b
richiex-jesus <matiasrichard300@gmail.com>
2025-12-11 17:49:07 +0800

    learnt a diff way to declare funcs for for loop

---
+++ loops-lesson/prime-numbers/prime-numbers.js
@@ -0,0 +1,41 @@
+function getUserInput() {
+    let userInput = prompt("Key in a value of n.");
+    return parseInt(userInput);
+}
+
+// console.log(getUserInput()); 
+// for debugging purposes only
+
+// function primeGenerator() {
+
+//     n = getUserInput();
+
+let n = getUserInput();
+
+primeGenerator:
+for (i = n; i >= 2; i--) { // for every value of i 
+    for (j = 2; j < i; j++) { // look for a divisor?
+        if (i % j == 0) {
+            continue primeGenerator;// not a prime, go next i??
+        } 
+    }
+    console.log( i )
+}
+
+// primeGenerator();
+
+// n = 9;
+
+// nextPrime:
+// for (let i = 2; i <= n; i++) { // for each i...
+
+//   for (let j = 2; j < i; j++) { // look for a divisor..
+//     if (i % j == 0) continue nextPrime; // not a prime, go next i
+//   }
+
+//   console.log( i ); // a prime
+// }
+
+
+
+
