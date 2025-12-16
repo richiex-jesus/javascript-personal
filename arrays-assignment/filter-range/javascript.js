efff4c755d12edb6cc4f1e61777359050e664d59
richiex-jesus <matiasrichard300@gmail.com>
2025-12-16 08:58:04 +0800

    defined filterRange with parameters arr, a and b

--- arrays-assignment/filter-range/javascript.js
+++ arrays-assignment/filter-range/javascript.js
@@ -3,12 +3,17 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let a = prompt("give a value of a between 1 to 10 inclusive.");
 let b = prompt("give a value of b between 1 to 10 inclusive. it must NOT be the same as a.")
 
-function filterRange(num) {
-    if (num >= a && num < b) {
-        return num
+function filterRange (arr, a, b) {
+    function filterActual(num) {
+        if (num >= a && num < b) {
+            return num
+        }
     }
+
+    let arrFiltered = arr.filter(filterActual);
+    console.log(arrFiltered);
+    console.log(arr)
 }
 
-let arrFiltered = arr.filter(filterRange);
-console.log(arrFiltered);
-console.log(arr)
+filterRange(arr, a, b);
+
