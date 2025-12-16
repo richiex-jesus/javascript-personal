f03934d5c0524a9f30b030f7ad4927b5e2b5cb2c
richiex-jesus <matiasrichard300@gmail.com>
2025-12-16 10:54:26 +0800

    completed with reference to solution, added comments for filter-in-place assignment

---
+++ arrays-assignment/filter-in-place/javascript.js
@@ -0,0 +1,16 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function filterRangeInPlace(arr, a, b) {
+    for (i = 0; i < arr.length; i++) {
+        let num = arr[i]
+        if (num < a || num > b) { // mistake earlier: used && operator instead of || operator. did a <= num <= b but problem is now splice can't be implemented.
+            arr.splice(i, 1);
+            i-- // what about i = 0? it works right?
+        }
+    }
+}
+
+filterRangeInPlace(arr, 6, 7);
+alert(arr);
+
+
