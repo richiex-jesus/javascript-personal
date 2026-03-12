f887a25838e6a5b428c82588f9b4cdf0f9ba825f
richiex-jesus <matiasrichard300@gmail.com>
2026-03-12 16:28:02 +0800

    applied euclidean algorithm

--- recursion-exercises/gcd/gcd.js
+++ recursion-exercises/gcd/gcd.js
@@ -8,7 +8,13 @@ function startGCD() {
 
 
 function gcd(a, b) { 
-    
+    if (a == 0 || b == 0) {
+        return console.log(0)
+    } else if (a % b == 0) {
+        return console.log(b)
+    } else {
+        return gcd(b, a % b)
+    }
 }
 
 startGCD();
@@ -19,3 +25,5 @@ startGCD();
 // firstly, my code is way too long.
 // secondly, i did not really make use of the base case scenario basis to try and generalise a pattern.
 // thirdly, it did not cover the cases where the denominators are very large. 
+
+// one algorithm I learnt today: euclidean algorithm :D 
