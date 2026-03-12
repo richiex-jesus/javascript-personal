32dd8691e243fe139d250e05b4d7fc38ac7bec31
richiex-jesus <matiasrichard300@gmail.com>
2026-03-12 18:08:14 +0800

    attempted range exercise

---
+++ recursion-exercises/range/range.js
@@ -0,0 +1,10 @@
+let a = Number(prompt("Input number, a:"));
+console.log(a)
+let b = Number(prompt("Input number, b:"));
+console.log(b)
+
+function range(a, b) {
+    
+}
+
+console.log(range(a, b));
