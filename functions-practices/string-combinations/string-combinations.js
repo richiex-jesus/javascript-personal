de4930745d2a0d5d90a052738e8230e7c703102f
richiex-jesus <matiasrichard300@gmail.com>
2026-05-21 18:30:30 +0800

    added for loop to wordArray

--- functions-practices/string-combinations/string-combinations.js
+++ functions-practices/string-combinations/string-combinations.js
@@ -4,6 +4,12 @@ function stringCombo(word) {
     let wordArray = word.split('')
     // check to see what the word looks like in an array!
     console.log(wordArray)
+    for (letter in wordArray) {
+        let index_number = wordArray.indexOf(letter)
+        for (i = index_number; i < (wordArray.length - 1); i++) {
+            print(letter + wordArray[index_number])
+        }
+    }
 }
 
 word = prompt("please input a random word:")
