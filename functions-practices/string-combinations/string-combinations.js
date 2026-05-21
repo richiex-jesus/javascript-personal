90fc4b0632d806366f248e5d099df2094cc78580
richiex-jesus <matiasrichard300@gmail.com>
2026-05-21 18:30:47 +0800

    added comments to for-loop in string-combinations.js

--- functions-practices/string-combinations/string-combinations.js
+++ functions-practices/string-combinations/string-combinations.js
@@ -4,10 +4,14 @@ function stringCombo(word) {
     let wordArray = word.split('')
     // check to see what the word looks like in an array!
     console.log(wordArray)
-    for (letter in wordArray) {
+    // make a loop that will be able to start with every possible letter in wordArray
+    for (letter in wordArray) { 
+        // this should give the index of the focused letter
         let index_number = wordArray.indexOf(letter)
-        for (i = index_number; i < (wordArray.length - 1); i++) {
-            print(letter + wordArray[index_number])
+        // a for-loop that will generate all possible letter combinations beginning with the focused letter
+        for (i = index_number; i < (wordArray.length); i++) {
+            // print out the result of the letter combination in the console.s
+            console.log(letter + wordArray[index_number])
         }
     }
 }
