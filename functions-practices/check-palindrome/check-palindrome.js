cb2d588fdadb3038dff92f75fd29aee290ee638e
richiex-jesus <matiasrichard300@gmail.com>
2026-05-21 17:56:22 +0800

    added comments to check-palindrome

--- functions-practices/check-palindrome/check-palindrome.js
+++ functions-practices/check-palindrome/check-palindrome.js
@@ -1,7 +1,11 @@
 function checkPalindrome(word) {
+    // following similar logic, you first split the given word into another array, called wordArray in this case.
     let wordArray = word.split('');
+    // then, reverse the order of elements in the array.
     wordArray.reverse();
+    // join the elements again to get the reversed word, assign to the variable, palindromeChecker
     let palindromeChecker = wordArray.join('')
+    // self-explanatory code.
     if (word === palindromeChecker) {
         console.log(true);
     } else {
