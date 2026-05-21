c4e216692f831ffa524dd8b91a986434b3769262
richiex-jesus <matiasrichard300@gmail.com>
2026-05-21 17:47:24 +0800

    added html doc to reverse-number

--- functions-practices/reverse-number/reverse-number.js
+++ functions-practices/reverse-number/reverse-number.js
@@ -1,12 +1,14 @@
 function reverseNum(num) {
     // split each digit in the number into an array
-    let numArray = num.split('')  
+    let numArray = num.toString().split('')  
     console.log(numArray)
     // reverse the order of the array, correspondingly reversing the digits of the number
     numArray.reverse();
     // join the elements (i.e. the digits) of the array together to get the reversed number as the answer!
-    let answer = numArray.join()
+    let answer = numArray.join('')
     console.log(answer)
 }
 
-reverseNum(12345);
+
+
+reverseNum(123456789);
