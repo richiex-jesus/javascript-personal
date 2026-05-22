aa34b7b522ccf24dde744115f45629ccb01a70ee
richiex-jesus <matiasrichard300@gmail.com>
2026-05-22 11:24:54 +0800

    trial and error for first-letter-caps

--- functions-practices/first-letter-caps/first-letter-caps.js
+++ functions-practices/first-letter-caps/first-letter-caps.js
@@ -2,13 +2,18 @@
 
 function firstLetterCaps(string) { 
     wordArray = string.split(' '); // this is done to split up by individual words rather than letters. 
+    newArray = [];
     for (index in wordArray) { // for every word in wordArray:
+        let word = wordArray[index]; // this is the word.
         wordLowerCase = word.toLowerCase(); // convert the entire word to lowercase first.
-        let wordLowerCaseSplit = wordLowerCase.split; // assign the split lowercase word to an array variable, which is now MUTABLE!
-        let wordFirstAlphabet = wordLowerCaseSplit[0]; // get the first alphabet of the array.
-        wordFirstAlphabet.toUpperCase() // this is the important step which supposedly converts the first letter to uppercase.
-        wordArray[index] = wordLowerCaseSplit.join(''); // join up the newly updated word.
+        let wordLowerCaseSplit = wordLowerCase.split(''); // assign the split lowercase word to an array variable, which is now MUTABLE!
+        (wordLowerCaseSplit[0]).toUpperCase() // this is the important step which supposedly converts the first letter to uppercase.
+        sortedWord = wordLowerCaseSplit.join(''); // join up the newly updated word.
         // update the word in the initial index of the old wordArray
+        newArray.push(sortedWord);
+        console.log(newArray)
+        return newArray;
+        
     }
     console.log(wordArray);
 }
